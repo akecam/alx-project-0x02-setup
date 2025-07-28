884ba009d21cfedf46d52636ea6a46b5a31bcb76
Donald Umeh <donaldumeh54@gmail.com>
2025-07-28 21:50:49 +0100

    implement a postcard component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,7 +1,8 @@
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 import React from "react";
 
-const Posts = () => {
+const Posts = ({ title, content, userId }: PostProps) => {
   return (
     <div>
       <Header />
