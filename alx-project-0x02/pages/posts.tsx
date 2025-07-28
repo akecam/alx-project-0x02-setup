9328477cdd344ce464487edc4e060b91c51c3340
Donald Umeh <donaldumeh54@gmail.com>
2025-07-28 21:58:02 +0100

    implement a postcard component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,13 +2,28 @@ import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
 import React from "react";
 
-const posts = () => {
+const posts = ({ posts }) => {
   return (
     <div>
       <Header />
-      <PostCard title="" content="" userId="" />
+      <PostCard
+        title={posts.title}
+        content={posts.content}
+        userId={posts.userId}
+      />
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("/data/posts");
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default posts;
