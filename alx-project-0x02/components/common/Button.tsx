64bddee35dae1e1be0dbf474a51ac557a9a49f89
Donald Umeh <donaldumeh54@gmail.com>
2025-07-28 21:40:13 +0100

    create and use a buttoon component

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,12 @@
+import { ButtonProps } from "@/interfaces";
+import React from "react";
+
+const Button = ({ size, shape }: ButtonProps) => {
+  return (
+    <div>
+      <button className={`${size} ${shape}`}>Button</button>
+    </div>
+  );
+};
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,14 @@
+import Button from "@/components/common/Button";
 import React from "react";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      <Button size="small" shape="rounded-sm" />
+      <Button size="medium" shape="rounded-md" />
+      <Button size="large" shape="rounded-full" />
+    </div>
+  );
 };
 
 export default about;
